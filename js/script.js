050f83fb22cd8d1c073f3ee648d81b956277e5cc
imPGupta <prashantkgupta17@gmail.com>
2020-01-20 00:41:00 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,27 @@
+$('.navbar-collapse a').click(function(){
+    $(".navbar-collapse").collapse('hide');
+});
+
+$(document).click(function(event) {
+    $(event.target).closest(".navbar").length || $(".navbar-collapse.show").length && $(".navbar-collapse.show").collapse("hide")
+  });
+
+$(document).ready(function () {
+
+    $('#sidebarCollapse').on('click', function () {
+        $('#sidebar').toggleClass('active');
+    });
+    
+});
+
+
+AOS.init({
+    offset:120,
+    duration:1000,
+});
+
+// scroll color
+$('body').scrollspy({
+    target: '#myNavbar',
+    offset: 250,
+  });
