c4ff01c0d27461d997936513578015676e8080f2
imPGupta <47490316+imPGupta@users.noreply.github.com>
2020-01-25 23:35:41 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-156947534-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-156947534-1');
+</script>
+
 $('.navbar-collapse a').click(function(){
     $(".navbar-collapse").collapse('hide');
 });
@@ -25,12 +35,4 @@ $('body').scrollspy({
     target: '#myNavbar',
     offset: 250,
   });
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-156947534-1"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
 
-  gtag('config', 'UA-156947534-1');
-</script>
