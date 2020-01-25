c8f3da41026ea8ba1b7be47b023109947ec11364
imPGupta <47490316+imPGupta@users.noreply.github.com>
2020-01-26 00:14:03 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,12 +1,3 @@
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-156947534-1"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'UA-156947534-1');
-</script>
 
 $('.navbar-collapse a').click(function(){
     $(".navbar-collapse").collapse('hide');
